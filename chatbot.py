6cbc3c684011d92773f99ed4da7c8a8f1f69142b
urjitjethwa <51492772+urjitjethwa@users.noreply.github.com>
2025-10-07 15:06:19 +0400

    chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,7 @@
 
 import streamlit as st
 
-with open("/content/Arabic/Arabic_Lesson-3-4.txt", encoding="utf-8") as f:
+with open("Arabic_Lesson-3-4.txt", encoding="utf-8") as f:
     arabic_text = f.read()
 st.session_state["arabic_text"] = arabic_text
 
